22f792dcf98fd979fd3bc514445bcbae00be44c0
JaspalSJ <jabbaljaspal@gmail.com>
2022-07-29 18:08:12 +0000

    Display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                alert(`You clicked ${gameType}`);
+                runGame(gaameType);
             }
-        })
+        });
     }
+
+    runGame("addition");
 })
 /**
- * The maian game "loop", called when the script is first loaded
+ * The main game "loop", called when the script is first loaded
  * and dafter the users answer has been processed
  */
-function runGame() {
-    let num1 = Math.floor(Math.randoom() * 25) + 1;
-    let num2 = Math.floor(Math.randoom() * 25) + 1;
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -38,7 +48,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
